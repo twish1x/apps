69bb57d7ce4c30781173ff4057af566680aa9ba7
twish1x <153032092+twish1x@users.noreply.github.com>
2024-07-19 19:02:52 -0700

    Create twish.bat

---
+++ twish.bat
@@ -0,0 +1,52 @@
+@echo off
+title Driver Disabler Via DevManView
+Echo Disabling Devices through DevManView
+devmanview /disable ""High Precision Event Timer""
+echo Disabled High Precision Event Timer
+devmanview /disable ""Microsoft GS Wavetable Synth""
+echo Disabled Microsoft GS Wavetable Synth
+devmanview /disable ""Microsoft RRAS Root Enumerator""
+echo Disabled "Microsoft RRAS Root Enumerator
+devmanview /disable ""Intel Management Engine""
+echo Disabled Intel Management Engine
+devmanview /disable ""Intel Management Engine Interface""
+echo Disabled Intel Manaaement Engine Interface
+devmanview /disable ""Intel SMBus""
+echo Disabled Intel SMBus
+devmanview /disable ""SM Bus Controller""
+echo Disabled SM Bus Controller
+devmanview /disable ""Amdlog""
+echo Disabled Amd Log
+devmanview /disable ""AMD PSP""
+echo Disabled Amd PSP
+devmanview /disable ""System Speaker""
+echo Disabled System Speaker
+devmanview /disable ""Composite Bus Enumerator""
+echo Disabled Composite Bus Enumerator
+devmanview /disable ""Microsoft Virtual Drive Enumerator""
+echo Disabled Microsoft Virtual Drive Enumerator
+devmanview /disable ""Microsoft Hyper-V Virtualization Infrastructure Driver""
+echo Disabled Microsoft Hyper-V Virtualization Infrastucture Driver
+devmanview /disable ""NDIS Virtual Network Adapter Enumerator""
+echo Disabled NDIS Virtual Network Adapter Enumerator
+devmanview /disable ""ote Desktop Device Redirector Bus""
+echo Disabled ote Desktop Device Redirector Bus
+devmanview /disable ""UMBus Root Bus Enumerator""
+echo Disabled UMBus Root Bus Enumerator
+devmanview /disable ""WAN Miniport (IP)""
+echo Disabled WAN Miniport (IP)
+devmanview /disable ""WAN Miniport (IKEv2)""
+echo Disabled WAN Miniport (IKEv2)
+devmanview /disable ""WAN Miniport (IPv6)""
+echo Disabled "WAN Miniport (IPv6)
+devmanview /disable ""WAN Miniport (L2TP)""
+echo Disabled WAN Miniport (L2TP)
+devmanview /disable ""WAN Miniport (PPPOE)""
+echo Disabled WAN Miniport (PPPOE)
+devmanview /disable ""WAN Miniport (PPTP)""
+echo Disabled WAN Miniport (PPTP)
+devmanview /disable ""WAN Miniport (SSTP)""
+echo Disabled WAN Miniport (SSTP)
+devmanview /disable ""WAN Miniport (Network Monitor)""
+echo Disabled WAN Miniport (Network Monitor)
+pause
