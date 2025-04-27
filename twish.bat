30d54fa9e18a63ab0cbccfdad9c6f95533013a38
twish1x <153032092+twish1x@users.noreply.github.com>
2025-04-26 20:44:13 -0700

    Update twish.bat

--- twish.bat
+++ twish.bat
@@ -1,170 +1,141 @@
 Title TwishAMD
 @echo off
-powershell Invoke-WebRequest ""https://github.com/twish1x/apps/raw/main/SCEWIN_64.exe"" -OutFile ""C:\Windows\SCEWIN_64.exe"" >nul 2>&1
-powershell Invoke-WebRequest ""https://github.com/twish1x/apps/raw/main/amifldrv64.sys"" -OutFile ""C:\Windows\amifldrv64.sys"" >nul 2>&1
-powershell Invoke-WebRequest ""https://github.com/twish1x/apps/raw/refs/heads/main/amigendrv64.sys"" -OutFile ""C:\Windows\amigendrv64.sys >nul 2>&1
-cd "C:\Windows"
+color C
 
-echo. Disabling C States...
 SCEWIN_64.exe /i /ms "Global C-state Control" /qv 0x0 /lang en-US
- 
-
- 
-echo. Disabling Fast Boot...
 SCEWIN_64.exe /i /ms "Fast Boot" /qv 0x0 /lang en-US
- 
-echo. Disabling Power On By Device...
 SCEWIN_64.exe /i /ms "Power On By Mouse" /qv 0x0 /lang en-US
 SCEWIN_64.exe /i /ms "Power On By Keyboard" /qv 0x0 /lang en-US
- 
-echo. Disabling Wake On Lan...
 SCEWIN_64.exe /i /ms "Wake on LAN" /qv 0x0 /lang en-US
- 
-echo. Enabling Bootup NumLock State...
 SCEWIN_64.exe /i /ms "Bootup NumLock State" /qv 0x1 /lang en-US
- 
-echo. Disabling Csm Support...
 SCEWIN_64.exe /i /ms "CSM Support" /qv 0x0 /lang en-US
- 
-echo. Disabling Network Stack Driver...
 SCEWIN_64.exe /i /ms "Network Stack Driver Support" /qv 0x0 /lang en-US
- 
-echo. Enabling XHCI Hand Off...
 SCEWIN_64.exe /i /ms "XHCI Hand-Off" /qv 0x0 /lang en-US
- 
-echo. Diasbling Legacy Usb Support...
 SCEWIN_64.exe /i /ms "Legacy USB Support" /qv 0x0 /lang en-US
- 
-echo. Disabling Resume By Alarm...
 SCEWIN_64.exe /i /ms "Resume by Alarm" /qv 0x0 /lang en-US
- 
-echo. Disabling Fullscreen Logo Display...
 SCEWIN_64.exe /i /ms "Full Screen LOGO Show" /qv 0x0 /lang en-US
- 
-echo. Disabling Security Device Support...
 SCEWIN_64.exe /i /ms "Security Device Support" /qv 0x0 /lang en-US
- 
-echo. Disabling CNVI Mode...
 SCEWIN_64.exe /i /ms "CNVI Mode" /qv 0x0 /lang en-US
- 
-echo. Disabling Energy Efficient P-state...
 SCEWIN_64.exe /i /ms "Energy Efficient P-state" /qv 0x0 /lang en-US
- 
-echo. Disabling Energy Performance Gain...
 SCEWIN_64.exe /i /ms "Energy Performance Gain" /qv 0x0 /lang en-US
- 
-echo. Disabling Energy Efficient Turbo...
 SCEWIN_64.exe /i /ms "Energy Efficient Turbo" /qv 0x0 /lang en-US
- 
-echo. Disabling BME DMA Mitigation...
 SCEWIN_64.exe /i /ms "BME DMA Mitigation" /qv 0x0 /lang en-US
- 
-echo. Disabling Enable RH Prevention...
 SCEWIN_64.exe /i /ms "Enable RH Prevention" /qv 0x0 /lang en-US
- 
-echo. Disabling Power Saving Features...
 SCEWIN_64.exe /i /ms "PEP TBT RP" /qv 0x0 /lang en-US
 SCEWIN_64.exe /i /ms "PEP LAN(GBE)" /qv 0x0 /lang en-US
 SCEWIN_64.exe /i /ms "PEP CSME" /qv 0x0 /lang en-US
 SCEWIN_64.exe /i /ms "PEP SDXCE" /qv 0x0 /lang en-US
 SCEWIN_64.exe /i /ms "PEP EMMC" /qv 0x0 /lang en-US
- 
-echo. Disabling IOMMU
 SCEWIN_64.exe /i /ms "IOMMU" /qv 0x0 /lang en-US
- 
-echo. Disabling AMD Cool'N'Quiet 
 SCEWIN_64.exe /i /ms "AMD Cool'N'Quiet" /qv 0x0 /lang en-US
- 
-echo. Disabling Serial / Parallel Port 
 SCEWIN_64.exe /i /ms "Serial / Parallel Port" /qv 0x0 /lang en-US
- 
- 
-
- 
-echo. Disabling "Chipset Power Saving Features" 
 SCEWIN_64.exe /i /ms "Chipset Power Saving Features" /qv 0x0 /lang en-US
- 
-echo. Disabling "Remote Display Feature"
 SCEWIN_64.exe /i /ms "Remote Display Feature" /qv 0x0 /lang en-US
- 
-echo. Disabling "Security Device Support" 
 SCEWIN_64.exe /i /ms "Security Device Support" /qv 0x0 /lang en-US
- 
-echo. Disabling ""PSS Support"" 
 SCEWIN_64.exe /i /ms "PSS Support" /qv 0x0 /lang en-US
- 
-echo. Disabling ""AB Clock Gating"" 
 SCEWIN_64.exe /i /ms "AB Clock Gating" /qv 0x0 /lang en-US
- 
-echo. Disabling ""PCIB Clock Run"" 
 SCEWIN_64.exe /i /ms "PCIB Clock Run" /qv 0x0 /lang en-US
- 
-echo. Disabling ""SR-IOV Support"" 
 SCEWIN_64.exe /i /ms "SR-IOV Support" /qv 0x0 /lang en-US
- 
-echo. Disabling ""Opcache Control"" 
 SCEWIN_64.exe /i /ms "Opcache Control" /qv 0x1 /lang en-US
- 
-echo. Disabling ""BME DMA Mitigation"" 
-SCEWIN_64.exe /i /ms "Opcache Control" /qv 0x0 /lang en-US
- 
-echo. Disabling ""Above 4G memory"" 
 SCEWIN_64.exe /i /ms "Above 4G memory" /qv 0x1 /lang en-US
- 
-echo. Disabling ""AB Clock Gating"" 
 SCEWIN_64.exe /i /ms "Adaptive S4" /qv 0x0 /lang en-US
- 
-echo. Disabling ""LAN Power Enable"" 
 SCEWIN_64.exe /i /ms "LAN Power Enable" /qv 0x0 /lang en-US
- 
-echo. Disabling ""PM L1 SS"" 
 SCEWIN_64.exe /i /ms "PM L1 SS" /qv 0x0 /lang en-US
- 
-echo. Disabling ""Win7 USB Wake Support"" 
 SCEWIN_64.exe /i /ms "Win7 USB Wake Support" /qv 0x0 /lang en-US
- 
-echo. Disabling ""AMD Cool&Quiet function"" 
 SCEWIN_64.exe /i /ms "AMD Cool&Quiet function" /qv 0x0 /lang en-US
- 
-echo. Disabling ""C6 Mode"" 
 SCEWIN_64.exe /i /ms "C6 Mode" /qv 0x0 /lang en-US
- 
-echo. Disabling ""PCIe Slot Configuration"" 
 SCEWIN_64.exe /i /ms "PCIe Slot Configuration" /qv 0x0 /lang en-US
- 
-echo. Disabling ""3DMark01 Enhancement"" 
 SCEWIN_64.exe /i /ms "3DMark01 Enhancement" /qv 0x0 /lang en-US
- 
-echo. Disabling ""Isochronous Support"" 
 SCEWIN_64.exe /i /ms "Isochronous Support" /qv 0x0 /lang en-US
- 
-echo. Disabling ""PS2 Devices Support"" 
 SCEWIN_64.exe /i /ms "PS2 Devices Support" /qv 0x0 /lang en-US
- 
-echo. Disabling ""Network Stack Driver Support"" 
 SCEWIN_64.exe /i /ms "Network Stack Driver Support" /qv 0x0 /lang en-US
- 
-
-
- 
-echo. Disabling ""Onboard PCIE LAN PXE ROM"" 
 SCEWIN_64.exe /i /ms "Onboard PCIE LAN PXE ROM" /qv 0x0 /lang en-US
- 
-echo. Disabling ""CRB test"" 
 SCEWIN_64.exe /i /ms "CRB test" /qv 0x0 /lang en-US
- 
-echo. Disabling ""Integrated Graphics"" 
 SCEWIN_64.exe /i /ms "Integrated Graphics" /qv 0x0 /lang en-US
- 
-echo. Disabling ""AB Clock Gating"" 
 SCEWIN_64.exe /i /ms "AB Clock Gating" /qv 0x0 /lang en-US
- 
-echo. Disabling ""PCIB Clock Run"" 
 SCEWIN_64.exe /i /ms "PCIB Clock Run" /qv 0x0 /lang en-US
- 
-echo. Disabling ""Aggressive Link PM Capability"" 
 SCEWIN_64.exe /i /ms "Aggressive Link PM Capability" /qv 0x0 /lang en-US
- 
-echo. Disabling ""Chipset Power Saving Features - "" 
 SCEWIN_64.exe /i /ms "Chipset Power Saving Features" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "LCLK DPM" /qv 0x2 /lang en-US
+SCEWIN_64.exe /i /ms "Discrete GPU's Audio" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Unused GPP Clocks Off" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Clock Power Management(CLKREQ#)" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "ACP Power Gating" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "ACP CLock Gating" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "DRAM Latency Enhance" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "Power Down Enable" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "ECO Mode" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "LN2 Mode" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "SoC/Uncore OC Mode" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "LCLK DPM Enhanced PCIe Detection" /qv 0x2 /lang en-US
+SCEWIN_64.exe /i /ms "SMEE" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "ACPI _CST C1 Declaration" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Indirect Branch Prediction Speculation" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Fast Short REP MOVSB" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "Enhanced REP MOVSB/STOSB" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "REP-MOV/STOS Streaming" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "Disable DF to external downstream IP SyncFloodPropagation" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "Disable DF sync flood propagation" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "Freeze DF module queues on error" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "CC6 memory region encryption" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "PSP error injection support" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "DRAM scrub time" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Poison scrubber control" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Redirect scrubber control" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "GMI encryption control" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "xGMI encryption control" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Data Poisoning" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "RCD Parity" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "DRAM Address Command Parity Retry" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Write CRC Enable" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "DRAM Write CRC Enable and Retry Limit" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "DRAM ECC Enable" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "DRAM UECC Retry" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "BankGroupSwap" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "BankGroupSwapAlt" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "Address Hash Bank" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Address Hash CS" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Address Hash Rm" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "DMA Protection" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "DMAr Support" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "ACS Enable" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Enable AER Cap" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "SRIS" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Determinism Control" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "Determinism Slider" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "APBDIS" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "DF Cstates" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Fixed SOC Pstate" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "CPPC" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "CPPC Preferred Cores" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "NBIO SyncFlood Generation" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "NBIO SyncFlood Reporting" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Log Poison Data from SLINK" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Edpc Control" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "ESPI Enable" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "ALink RAS Support" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "S3/Modern Standby Support" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "DRAM map inversion" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "SVM Mode" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "TSME" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "PPC Adjustment" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Int. Clk Differential Spread" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "SATA Slumber State Capability" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "SB Clock Spread Spectrum" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "ACPI SLIT remote relative distance" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "SPD Read Optimization" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "PCIe Ten Bit Tag Support" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "NBIO DPM Control" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "NBIO Poison Consumption" /qv 0x2 /lang en-US
+SCEWIN_64.exe /i /ms "NBIO RAS Global Control" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "NBIO RAS Control" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Sata RAS Support" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Aggresive SATA Device Sleep Port 0" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Aggresive SATA Device Sleep Port 1" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Socket1 DevSlp0 Enable" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "Socket1 DevSlp1 Enable" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "ACPI SRAT L3 Cache As NUMA Domain" /qv 0x1 /lang en-US
+SCEWIN_64.exe /i /ms "Periodic Directory Rinse" /qv 0x0 /lang en-US
+SCEWIN_64.exe /i /ms "AB Clock Gating" /qv 0x0 /lang en-US
+echo. Finished Enjoy!
 del "%~f0"
